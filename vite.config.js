ca3cc8e0de5ab41463cbe5f73307cf3be55c1589
camove <ovidiuecampean@gmail.com>
2025-06-16 20:23:53 +0300

    hai

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-   build: {
+  build: {
     outDir: "dist",
+    target: "esnext",  // Asigură compatibilitatea cu modulele ES6
+    assetsInlineLimit: 0,  // Previne încapsularea resurselor în Base64
   },
-})
-
+});
 
