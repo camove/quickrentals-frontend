8910edbedad713ed2e5c6b5a9010a0374fa5e9c7
camove <ovidiuecampean@gmail.com>
2025-06-16 19:58:06 +0300

    delete global.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RoutesStructure from "./Routes/RoutesStructure";
 import { AuthProvider } from "./Context/AuthContext";
-import './styles/globals.css';
+// import './styles/globals.css';
 
 const router = createBrowserRouter(RoutesStructure);
 
